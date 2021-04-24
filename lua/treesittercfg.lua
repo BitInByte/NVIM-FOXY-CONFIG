864c79d106051a7b3f8895e6cd6203cdd742769c
BitInByte <bitinbyte@outlook.com>
2021-04-24 23:06:12 +0100

    Added languages parsers to install in treesitter

--- lua/treesittercfg.lua
+++ lua/treesittercfg.lua
@@ -1,1 +1,4 @@
-require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "java", "vue", "javascript", "typescript", "css", "lua", "graphql", "html", "tsx", "json", "python", "latex", "php", "bibtex", "scss" },
+  highlight = { enable = true }
+}
